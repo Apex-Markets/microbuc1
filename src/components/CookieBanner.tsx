72a21c2eabe5e6f626ac344bbab6f16bfd87eb3c
s <info@elitepictures.co>
2026-01-23 22:39:27 -0800

    Remove '12,000+ u

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -14,7 +14,7 @@ function CookieBanner() {
   return (
     <div className="cookie-banner">
       <p>
-        Cookies help us keep our website safe and give you a better experience. By visiting our website, you agree to our use of essential cookies.&nbsp;
+        Cookies help us keep our website safe and give you a better experience. By visiting our website, you agree to our use of essential cookies.
         With your consent, we also use analytics cookies to serve personalized ads or content and analyze our traffic.
       </p>
       <button onClick={acceptCookies}>Accept</button>
@@ -22,21 +22,4 @@ function CookieBanner() {
   );
 }
 
-export default function CookieBanner() {
-  return (
-    <div style={{
-      position: "fixed",
-      left: 0,
-      right: 0,
-      bottom: 0,
-      background: "#d72638",
-      color: "#fff",
-      padding: "2rem",
-      zIndex: 99999,
-      textAlign: "center",
-      fontSize: "1.5rem",
-    }}>
-      TEST BANNER. If you see this, your component is being rendered!
-    </div>
-  );
-}
+export default CookieBanner;
