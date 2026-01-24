318cf2a7976a10995c87f49dccd058be0029277b
s <info@elitepictures.co>
2026-01-23 22:24:46 -0800

    Remove '12,000+ users' from HeroSection copy

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -13,7 +13,10 @@ function CookieBanner() {
 
   return (
     <div className="cookie-banner">
-      <p>We use cookies to ...</p>
+      <p>
+        Cookies help us keep our website safe and give you a better experience. By visiting our website, you agree to our use of essential cookies.&nbsp;
+        With your consent, we also use analytics cookies to serve personalized ads or content and analyze our traffic.
+      </p>
       <button onClick={acceptCookies}>Accept</button>
     </div>
   );
